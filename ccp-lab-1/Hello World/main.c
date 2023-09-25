638438d6aa603e3967666bb6487c7933e695d45f
AnhMinh6 <145763860+AnhMinh6@users.noreply.github.com>
2023-09-25 07:35:29 +0300

    Update main.c

--- ccp-lab-1/Hello World/main.c
+++ ccp-lab-1/Hello World/main.c
@@ -1,14 +1,7 @@
-//
-//  main.c
-//  Hello World
-//
-//  Created by Winslett on 25.09.2023.
-//
-
-#include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
-}
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout <<"Hello, world!";
+	return 0;
+ }
