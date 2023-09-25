f8bbbd9ce2ab15d34e83e9fe71f932eb29e38e4b
AnhMinh6 <145763860+AnhMinh6@users.noreply.github.com>
2023-09-25 07:23:34 +0300

    first commit

---
+++ ccp-lab-1/Hello World/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Hello World
+//
+//  Created by Winslett on 25.09.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
